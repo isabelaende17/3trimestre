eefbddf1da17c2ba896e437acfd33e4eaf14e9e5
isabelaende17 <162144251+isabelaende17@users.noreply.github.com>
2024-10-07 12:16:22 -0300

    ,p

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,12 @@ criaCartao(
     'O que é programação?',
     'Programação é o processo de escrita, teste e manutenção de um programa de computador',
 )
+criaCartao(
+    'JavaScript',
+    'Para o que serve o JavaScript?',
+    'Serve para implementar itens complexos em páginas web',
+)
+criaCartao(
+    'Quando usar o phyton?',
+    'Podemos usar em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning'
+)
